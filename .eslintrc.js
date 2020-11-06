f74c7ca10f4dd333880e28d5969a28fd532b46fb
CyanSalt <c.salt@hotmail.com>
2020-11-06 16:15:43 +0800

    chore: update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,5 @@ module.exports = {
   root: true,
   extends: [
     '@cyansalt',
-    '@cyansalt/eslint-config/import',
-    '@cyansalt/eslint-config/unicorn',
-    '@cyansalt/eslint-config/vue/v3',
   ],
 }
