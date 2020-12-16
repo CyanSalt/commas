b200c6c8a2f522ef40d6ab4c906a4df067c83810
CyanSalt <c.salt@hotmail.com>
2020-12-16 11:31:09 +0800

    feat: refine the app icon

--- makefile.js
+++ makefile.js
@@ -1,11 +1,28 @@
 const childProcess = require('child_process')
 const fs = require('fs')
+const path = require('path')
 const util = require('util')
+const chalk = require('chalk')
 const packager = require('electron-packager')
 const png2icons = require('png2icons')
 const app = require('./package.json')
 
 const execa = util.promisify(childProcess.exec)
+
+const logger = {
+  info(message) {
+    console.log(chalk.inverse(chalk.blue(' INFO ')) + ' ' + message)
+  },
+  done(message) {
+    console.log(chalk.inverse(chalk.green(' DONE ')) + ' ' + message)
+  },
+  warn(message) {
+    console.log(chalk.inverse(chalk.yellow(' WARN ')) + ' ' + chalk.yellow(message))
+  },
+  error(message) {
+    console.log(chalk.inverse(chalk.red(' ERROR ')) + ' ' + chalk.red(message))
+  },
+}
 
 async function generateAppIcon(input, icon, suffix) {
   // Check icon file
@@ -17,7 +34,7 @@ async function generateAppIcon(input, icon, suffix) {
     // ignore error
   }
   try {
-    console.info(`Generating ${suffix.toUpperCase()} icon for application...`)
+    logger.info(`Generating ${suffix.toUpperCase()} icon for application...`)
     const builder = suffix === 'icns' ? png2icons.createICNS : png2icons.createICO
     const output = builder(input, png2icons.BICUBIC2, 0, false, true)
     await fs.promises.writeFile(iconPath, output)
@@ -63,12 +80,22 @@ async function getMacOSCodeSign(name) {
 
 async function copyWindowsManifest(dir) {
   try {
-    console.info(`Copying Visual Elements Manifest for Windows...`)
+    logger.info(`Copying Visual Elements Manifest for Windows...`)
     const manifest = `${app.name}.VisualElementsManifest.xml`
     await fs.promises.copyFile(`resources/${manifest}`, `${dir}/${manifest}`)
   } catch {
     // ignore error
   }
+}
+
+async function compressPackage(dir) {
+  logger.info(`Packing ${path.basename(dir)}...`)
+  try {
+    await fs.promises.unlink(`${dir}.zip`)
+  } catch {
+    // ignore error
+  }
+  return execa(`zip -ry ${dir}.zip ${dir}/`)
 }
 
 async function make() {
@@ -93,12 +120,13 @@ async function make() {
       if (dir.includes('win32')) {
         await copyWindowsManifest(dir)
       }
+      return compressPackage(dir)
     })
   )
   return Date.now() - startedAt
 }
 
 make().then(
-  duration => console.info(`Build finished after ${duration / 1000}s.`),
-  err => console.error(err),
+  duration => logger.done(`Build finished after ${duration / 1000}s.`),
+  err => logger.error(err),
 )
