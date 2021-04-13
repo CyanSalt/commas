c1f9a0b927a2b94ba0d76bd2d7826e3ba1c48c0c
CyanSalt <c.salt@hotmail.com>
2021-04-13 21:02:02 +0800

    fix: sudo prompt icon on macOS

--- main/utils/privilige.ts
+++ main/utils/privilige.ts
@@ -1,12 +1,14 @@
+import * as path from 'path'
 import { app } from 'electron'
 import * as sudo from 'sudo-prompt'
-import { resources } from '../utils/directory'
 
 const sudoExeca = (command: string) => {
   return new Promise((resolve, reject) => {
     sudo.exec(command, {
       name: app.name,
-      icns: resources.file('images/icon.icns'),
+      icns: process.platform === 'darwin'
+        ? path.join(app.getPath('exe'), '../../Resources/electron.icns')
+        : undefined,
     }, (err, stdout, stderr) => {
       if (err) reject(Object.assign(err, { stdout, stderr }))
       else resolve({ stdout, stderr })
