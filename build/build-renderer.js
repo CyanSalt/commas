7800f91e88a26f5d501d0f2fc05edd6cfbb19258
CyanSalt <c.salt@hotmail.com>
2021-06-24 10:46:28 +0800

    refactor: compile to esmodule

--- build/build-renderer.js
+++ build/build-renderer.js
@@ -27,14 +27,49 @@ const importGlob = () => ({
   },
 })
 
-const wrapIIFE = () => ({
-  name: 'wrap-iife',
-  generateBundle(options, bundle) {
-    for (const chunk of Object.values(bundle)) {
-      if (chunk.code) {
-        chunk.code = `(function(){\n${chunk.code}\n})()`
+const dynamicRequire = ({ moduleIds }) => ({
+  name: 'dynamic-require',
+  enforce: 'post',
+  transform(code) {
+    const ast = this.parse(code)
+    let lastIndex = 0
+    let transformedCode = ''
+    let dynamicRequireCode = ''
+    let endIndex = 0
+    const importDeclarations = ast.body
+      .filter(statement => statement.type === 'ImportDeclaration')
+    if (importDeclarations.length) {
+      endIndex = importDeclarations[importDeclarations.length - 1].end
+    }
+    for (const statement of importDeclarations) {
+      if (statement.type === 'ImportDeclaration') {
+        const source = statement.source
+        if (source.type === 'Literal' && moduleIds.includes(source.value)) {
+          transformedCode = code.slice(lastIndex, statement.start)
+          lastIndex = statement.end
+          const namespaceSpecifier = statement.specifiers
+            .find(specifier => specifier.type === 'ImportNamespaceSpecifier')
+          if (namespaceSpecifier) {
+            dynamicRequireCode += `\nconst ${namespaceSpecifier.local.name} = require(${source.raw});`
+          }
+          const normalSpecifiers = statement.specifiers
+            .filter(specifier => specifier.type === 'ImportSpecifier')
+          if (normalSpecifiers.length) {
+            dynamicRequireCode += `\nconst { ${
+              normalSpecifiers.map(specifier => (specifier.imported.name === specifier.local.name ? specifier.imported.name : `${specifier.imported.name}: ${specifier.local.name}`)).join(', ')
+            } } = require(${source.raw});`
+          }
+        }
       }
     }
+    if (dynamicRequireCode) {
+      transformedCode += code.slice(lastIndex, endIndex)
+      transformedCode += dynamicRequireCode
+      transformedCode += code.slice(endIndex)
+    } else {
+      transformedCode += code.slice(lastIndex)
+    }
+    return transformedCode
   },
 })
 
@@ -52,7 +87,12 @@ module.exports = () => build({
   plugins: [
     vue(),
     importGlob(),
-    wrapIIFE(),
+    dynamicRequire({
+      moduleIds: [
+        ...builtinModules,
+        'electron',
+      ],
+    }),
   ],
   json: {
     stringify: true,
@@ -69,9 +109,15 @@ module.exports = () => build({
         freeze: false,
       },
     },
+    commonjsOptions: {
+      ignore: [
+        ...builtinModules,
+        'electron',
+      ],
+    },
     lib: {
       entry: 'app.ts',
-      formats: ['cjs'],
+      formats: ['es'],
       fileName: 'app',
     },
   },
