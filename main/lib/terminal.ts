16d9d0115f01318162ac1991b312cf919463149d
CyanSalt <c.salt@hotmail.com>
2021-07-26 16:41:59 +0800

    docs: add latest version comment

--- main/lib/terminal.ts
+++ main/lib/terminal.ts
@@ -118,6 +118,7 @@ function handleTerminalMessages() {
   })
   ipcMain.handle('get-git-branch', async (event, directory: string) => {
     const command = process.platform === 'win32'
+      // TODO: os.devNull
       ? 'git rev-parse --abbrev-ref HEAD 2> NUL'
       : 'git branch 2> /dev/null | grep \\* | cut -d " " -f2'
     try {
