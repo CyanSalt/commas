4ad5ae90207395fa50665ca851e745b5afb14b87
CyanSalt <c.salt@hotmail.com>
2022-02-14 14:27:19 +0800

    refactor: rename core API

--- api/core-main.ts
+++ api/core-main.ts
@@ -1,7 +1,7 @@
 import * as raw from './main'
 import * as addon from './modules/addon'
 
-export const proxy = addon.cloneAPI(raw, 'commas')
+export const proxy = addon.cloneAPI(raw, 'terminal')
 
 export {
   addon,
--- api/core-renderer.ts
+++ api/core-renderer.ts
@@ -1,7 +1,7 @@
 import * as addon from './modules/addon'
 import * as raw from './renderer'
 
-export const proxy = addon.cloneAPI(raw, 'commas')
+export const proxy = addon.cloneAPI(raw, 'terminal')
 
 export {
   addon,
