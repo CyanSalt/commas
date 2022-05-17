01d71d92d4e8629f6915f5a476970c92e5daa87c
CyanSalt <c.salt@hotmail.com>
2022-05-17 14:49:02 +0800

    fix(power-mode): add hint message

--- addons/power-mode/src/main/index.ts
+++ addons/power-mode/src/main/index.ts
@@ -4,6 +4,16 @@ commas.context.provide('cli', {
   command: 'power',
   async handler({ argv }, event) {
     const [status] = argv
-    event.sender.send('toggle-power-mode', status !== 'off')
+    const enabled = status !== 'off'
+    event.sender.send('toggle-power-mode', enabled)
+    if (enabled) {
+      return `
+Power mode is turned on. Enter
+
+    commas power off
+
+to exit power mode.
+`.trim()
+    }
   },
 })
