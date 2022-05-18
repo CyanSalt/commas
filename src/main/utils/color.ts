71b352c600f1c1801f4b3adb7a5405b835c020a7
CyanSalt <c.salt@hotmail.com>
2022-05-18 14:46:43 +0800

    refactor: use color-convert

--- src/main/utils/color.ts
+++ src/main/utils/color.ts
@@ -1,3 +1,4 @@
+import * as conversions from 'color-convert'
 import parseRGBA from 'color-rgba'
 
 interface RGBA {
@@ -25,82 +26,13 @@ interface HSLA {
 }
 
 function toHSLA(rgba: RGBA): HSLA {
-  const r = rgba.r / 255
-  const g = rgba.g / 255
-  const b = rgba.b / 255
-  const min = Math.min(r, g, b)
-  const max = Math.max(r, g, b)
-  const delta = max - min
-
-  let h = 0
-  if (max === min) {
-    // pass
-  } else if (r === max) {
-    h = (g - b) / delta
-  } else if (g === max) {
-    h = 2 + (b - r) / delta
-  } else if (b === max) {
-    h = 4 + (r - g) / delta
-  }
-  h = Math.min(Math.round(h * 60), 360)
-  if (h < 0) {
-    h += 360
-  }
-
-  const l = (min + max) / 2
-
-  let s: number
-  if (l <= 0.5) {
-    s = delta / (max + min)
-  } else {
-    s = delta / (2 - max - min)
-  }
-
-  return { h, s, l, a: rgba.a }
+  const [h, s, l] = conversions.rgb.hsl([rgba.r, rgba.g, rgba.b])
+  return { h, s: s / 100, l: l / 100, a: rgba.a }
 }
 
 function toRGBAFromHSLA(hsla: HSLA): RGBA {
-  const h = hsla.h / 360
-  const s = hsla.s
-  const l = hsla.l
-
-  if (s === 0) {
-    const value = Math.round(l * 255)
-    return { r: value, g: value, b: value, a: hsla.a }
-  }
-
-  let t2
-  if (l < 0.5) {
-    t2 = l * (1 + s)
-  } else {
-    t2 = l + s - l * s
-  }
-  const t1 = 2 * l - t2
-
-  const rgb = [0, 0, 0]
-  let t3
-  for (let i = 0; i < 3; i += 1) {
-    t3 = h + 1 / 3 * (1 - i)
-    if (t3 < 0) {
-      t3 += 1
-    }
-    if (t3 > 1) {
-      t3 -= 1
-    }
-    let value: number
-    if (6 * t3 < 1) {
-      value = t1 + (t2 - t1) * 6 * t3
-    } else if (2 * t3 < 1) {
-      value = t2
-    } else if (3 * t3 < 2) {
-      value = t1 + (t2 - t1) * (2 / 3 - t3) * 6
-    } else {
-      value = t1
-    }
-    rgb[i] = Math.round(value * 255)
-  }
-
-  return { r: rgb[0], g: rgb[1], b: rgb[2], a: hsla.a }
+  const [r, g, b] = conversions.hsl.rgb([hsla.h, hsla.s * 100, hsla.l * 100])
+  return { r, g, b, a: hsla.a }
 }
 
 function toCSSColor(rgba: RGBA) {
