fbea1a81690a52d1d6f063843cfc10f5c648e126
CyanSalt <c.salt@hotmail.com>
2023-05-15 15:59:50 +0800

    fix: devicon format

--- build/utils/postcss-radicalize-font-face.mjs
+++ build/utils/postcss-radicalize-font-face.mjs
@@ -1,3 +1,19 @@
+import parser from 'css-font-face-src'
+
+/**
+ * @template T
+ * @param {T[]} values
+ * @param {((value: T, index: number, obj: T[]) => unknown)[]} predicates
+ * @returns {number}
+ */
+function findAnyIndexOf(values, predicates) {
+  for (const predicate of predicates) {
+    const index = values.findIndex(predicate)
+    if (index !== -1) return index
+  }
+  return -1
+}
+
 /**
  * @type {import('postcss').PluginCreator}
  */
@@ -6,12 +22,23 @@ const plugin = () => {
     postcssPlugin: 'postcss-radicalize-font-face',
     Once: root => {
       root.walkAtRules('font-face', atRule => {
+        let lastRule
         atRule.walkDecls('src', rule => {
-          const srcset = rule.value.split(/\s*,\s*/)
-          const firstURLIndex = srcset.findIndex(item => /\burl\(/.test(item))
-          if (firstURLIndex !== -1) {
-            rule.value = srcset.slice(0, firstURLIndex + 1).join(', ')
+          if (lastRule) {
+            lastRule.remove()
           }
+          const srcset = parser.parse(rule.value)
+          const matchedIndex = findAnyIndexOf(srcset, [
+            item => item.format === 'woff2',
+            item => item.format === 'woff',
+            item => item.url,
+          ])
+          if (matchedIndex !== -1) {
+            rule.value = parser.serialize(srcset.filter((item, index) => {
+              return !item.url || index === matchedIndex
+            }))
+          }
+          lastRule = rule
         })
       })
     },
