621fac276a679508fca198a7e1d34a5f70daa742
CyanSalt <c.salt@hotmail.com>
2023-10-27 17:29:52 +0800

    fix: a11y support

--- src/main/lib/a11y.ts
+++ src/main/lib/a11y.ts
@@ -1,11 +1,25 @@
 import { app } from 'electron'
 import { provideIPC } from '../utils/compositions'
 
-const a11yEnabled = $computed({
-  get: () => app.isAccessibilitySupportEnabled(),
-  set: value => {
-    app.setAccessibilitySupportEnabled(value)
-  },
+const a11yEnabled = $customRef((track, trigger) => {
+  let enabled = false
+  app.on('ready', () => {
+    enabled = app.accessibilitySupportEnabled
+    trigger()
+  })
+  app.on('accessibility-support-changed', (event, accessibilitySupportEnabled) => {
+    enabled = accessibilitySupportEnabled
+    trigger()
+  })
+  return {
+    get() {
+      track()
+      return enabled
+    },
+    set(value) {
+      app.accessibilitySupportEnabled = value
+    },
+  }
 })
 
 function handleA11yMessages() {
