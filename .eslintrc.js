aa67794314c6d9b3128837ab66bef9d5ce21ca37
CyanSalt <c.salt@hotmail.com>
2023-10-30 20:02:57 +0800

    chore: clean rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     project: './tsconfig.tools.json',
   },
   rules: {
-    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
     'vue/no-undef-components': 'error',
     'vue/no-undef-properties': 'error',
   },
