9a1c196de3836236005721593c43aedc21966f3a
CyanSalt <c.salt@hotmail.com>
2024-05-20 15:32:53 +0800

    chore: update lint config

--- stylelint.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: [
-    '@cyansalt/stylelint-config-preset',
-  ],
-}
---
+++ stylelint.config.mjs
@@ -0,0 +1,5 @@
+import config from '@cyansalt/stylelint-config'
+
+export default {
+  overrides: config(),
+}
