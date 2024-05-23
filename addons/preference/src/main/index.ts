a1867c18a9de9cba52877d4243e889c8b846d4ca
CyanSalt <c.salt@hotmail.com>
2024-05-23 21:22:54 +0800

    fix(preference): open from menu

--- addons/preference/src/main/index.ts
+++ addons/preference/src/main/index.ts
@@ -4,13 +4,24 @@ import { ipcMain, shell } from 'electron'
 export default () => {
 
   ipcMain.removeHandler('open-settings')
+  commas.context.removeHandler('global:open-settings')
 
   commas.ipcMain.handle('open-settings', event => {
     event.sender.send('open-preference-pane')
   })
 
+  const focusedWindow = $(commas.frame.useFocusedWindow())
+  commas.context.handle('global:open-settings', () => {
+    if (focusedWindow) {
+      focusedWindow.webContents.send('open-preference-pane')
+    }
+  })
+
   commas.app.onCleanup(() => {
     ipcMain.handle('open-settings', () => {
+      return commas.settings.openSettingsFile()
+    })
+    commas.context.handle('global:open-settings', () => {
       return commas.settings.openSettingsFile()
     })
   })
--- api/modules/frame.ts
+++ api/modules/frame.ts
@@ -1,9 +1,10 @@
-import { broadcast } from '../../src/main/lib/frame'
+import { broadcast, useFocusedWindow } from '../../src/main/lib/frame'
 import { notify } from '../../src/main/utils/notification'
 
 export * from '../shim'
 
 export {
+  useFocusedWindow,
   broadcast,
   notify,
 }
