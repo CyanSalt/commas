b2543220bc3c0435c5b08c0dfb13b43e1f20d2fd
CyanSalt <c.salt@hotmail.com>
2025-01-02 14:10:50 +0800

    chore: refine electron versions script

--- build/utils/electron-versions.cjs
+++ build/utils/electron-versions.cjs
@@ -1,4 +1,3 @@
 const os = require('node:os')
 
 process.stdout.write(JSON.stringify(process.versions) + os.EOL)
-process.exit()
--- build/utils/get-versions.mjs
+++ build/utils/get-versions.mjs
@@ -1,10 +1,11 @@
 import path from 'node:path'
+import electronPath from 'electron'
 import { execa } from './child-process.mjs'
 import { getDirectory } from './common.mjs'
 
 export default async function () {
   const versionScript = path.resolve(getDirectory(import.meta), 'electron-versions.cjs')
-  const { stdout: versions } = await execa(`npx electron ${versionScript}`, {
+  const { stdout: versions } = await execa(`${electronPath} ${versionScript}`, {
     env: {
       ...process.env,
       ELECTRON_RUN_AS_NODE: '1',
