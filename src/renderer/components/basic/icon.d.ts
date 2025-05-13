0028e8694ba20856083094f8d0828179e466b93c
CyanSalt <c.salt@hotmail.com>
2025-05-13 15:48:14 +0800

    fix: typing depth

--- src/renderer/components/basic/icon.d.ts
+++ src/renderer/components/basic/icon.d.ts
@@ -1,8 +1,10 @@
-import type * as lucide from 'lucide-static'
-import type { KebabCase } from 'type-fest'
+// import type * as lucide from 'lucide-static'
+// import type { KebabCase } from 'type-fest'
 
-export type LucideIcon = keyof {
-  [K in keyof typeof lucide as `lucide-${KebabCase<K>}`]: unknown
-}
+// TODO: too deep
+// export type LucideIcon = keyof {
+//   [K in keyof typeof lucide as `lucide-${KebabCase<K>}`]: unknown
+// }
+export type LucideIcon = `lucide-${string}`
 
 export type SimpleIconsIcon = `simple-icons-${string}`
