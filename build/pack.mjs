75e374bf51de18b4bd5f97d2fec605714bdac7aa
CyanSalt <c.salt@hotmail.com>
2025-07-28 22:10:31 +0800

    fix: ignore deep node_modules

--- build/pack.mjs
+++ build/pack.mjs
@@ -111,7 +111,7 @@ const options = {
   icon: 'resources/images/icon.png',
   ignore: [
     /^\/(?!addons|dist|node_modules|resources|package\.json)/,
-    /^\/addons\/[^/]+\/src/,
+    /^\/addons\/[^/]+\/(node_modules|src)/,
     /^\/resources\/.*\.(ico|icns)$/,
   ],
   extraResource: [
